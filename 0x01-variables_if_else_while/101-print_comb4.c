8f0d9f8d27376b113b44ccd7ff8dfedf2908ed24
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-02-18 03:54:20 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,15 +1,39 @@
-#include <stdlib.h>
-/* ok */
-#include <time.h>
-/* more headers goes there */
+/*
+ * File: 102-print_comb5.c
+ * oh
+ */
 
-/* betty style doc for function main goes there */
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int n;
+	int num1, num2;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
