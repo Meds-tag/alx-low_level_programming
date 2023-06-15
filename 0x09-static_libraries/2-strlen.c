6f690d8829a67b8287516a6a459bfcf619d006a6
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-06-15 02:22:58 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,10 +1,18 @@
 #include "main.h"
 /**
  * _strlen - returns the length of a string
- * @s:input value
-* Return: value of s
+ * @s: string
+ * Return: length
  */
-int _strlen(int *s)
+int _strlen(char *s)
 {
-	return (s);
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
