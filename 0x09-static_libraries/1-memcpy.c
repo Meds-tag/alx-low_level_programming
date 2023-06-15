c5d5b902c9acb50e43b03509b12d6829e7593a25
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-06-15 02:07:35 +0100

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_memcpy - a function that
- *@dest : no
- *Return : no
+  *@dest: memory where is stored
+ *Return: copied memory with n byted changed
  */
 char *_memcpy(char *dest)
 {
