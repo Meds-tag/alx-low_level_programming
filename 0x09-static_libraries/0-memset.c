4986ba53956ded444bd2d997075ebf22df3c7e2e
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-06-15 02:08:08 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _memset - fill a block of memory with a specific value
-  * @s: starting address of memory to be filled
+ * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
- * Return
+ * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
