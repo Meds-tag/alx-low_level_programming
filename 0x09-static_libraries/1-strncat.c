e0a31d7c342514d2a1af31d0eb08ca573c02173d
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-06-15 02:10:20 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -2,8 +2,10 @@
 #include "main.h"
 /**
  * _strncat - concatenate two strings
- * 
- * Return
+ * @dest : inout value
+ * Return : the input value
  */
 char *_strncat(char *dest)
-{return(dest)}
+{
+  return(dest)
+}
