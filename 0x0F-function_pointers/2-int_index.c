e5c4f0208c06d4f15055394748216a5df0b3a3a9
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-06-22 03:48:48 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 /**
- * int_index - returns the index of the first element for which the cmp function does not return 0
- * @array: array of elements
+ * int_index - returns index of the first element for which the cmp != 0
+ * @array: array
  * @size: size of the array
- * @cmp: pointer to the function to be used to compare values
- * Return: nothing
+ * @cmp: pointer to a function
+ * Return: NULL
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
