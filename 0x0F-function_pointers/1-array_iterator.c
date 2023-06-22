860a2a9abd6ce1a860d0d3550785ed307a5bacc3
Meds-tag <123335821+Meds-tag@users.noreply.github.com>
2023-06-22 03:35:49 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,14 @@
  * @action: is a pointer to the function you need to use
  * Return: void
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
 	if (array == NULL || action == NULL)
 		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
